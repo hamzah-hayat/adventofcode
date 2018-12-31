2efecc4535d1a47bdeebf6bf5531636ce9c85c1b
hamzah-hayat <hamzahhayat@live.co.uk>
2018-12-31 15:59:38 +0000

    Work on Part one

--- GuardDuty-4/main.go
+++ GuardDuty-4/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -19,10 +20,56 @@ func main() {
 }
 
 func PartOne() {
-	input := readInput()
-	for _, line := range input {
-		fmt.Println(line)
+	shifts := readInput()
+
+	// Find out who the most sleepy guard is
+	guardSleepSum := make(map[string]int)
+	for _, shift := range shifts {
+		// Build a map and figure out which guard is the most sleep
+		for _, sleepValue := range shift.sleep {
+			if sleepValue {
+				guardSleepSum[shift.guardID]++
+			}
+		}
 	}
+
+	//Find sleepiest guard
+	highestSum := 0
+	highestGuard := ""
+	for guardID, sleepSum := range guardSleepSum {
+		if sleepSum > highestSum {
+			highestGuard = guardID
+			sleepSum = highestSum
+		}
+	}
+
+	fmt.Println("The sleepiest guard is " + highestGuard)
+
+	// Find out when this guard was the sleepiest
+	guardMinuteSleepSum := make([]int, 60)
+	for _, shift := range shifts {
+		if shift.guardID == highestGuard {
+			for min, val := range shift.sleep {
+				if val {
+					guardMinuteSleepSum[min]++
+				}
+			}
+		}
+	}
+
+	fmt.Println(guardMinuteSleepSum)
+
+	highestMinuteSum := 0
+	highestMinute := 0
+	for minute, sleepMinSum := range guardMinuteSleepSum {
+		if sleepMinSum > highestMinuteSum {
+			highestMinute = minute
+			highestMinuteSum = sleepMinSum
+		}
+	}
+
+	highestMinuteStr := strconv.Itoa(highestMinute)
+	fmt.Println("They were sleepiest during minute " + highestMinuteStr)
 
 }
 
@@ -66,14 +113,40 @@ func BuildShift(i int, input []string) (shift, int) {
 	// Return the new shift and the next i
 	var newShift shift
 
+	// Get the GuardID for this shift
 	newShift.guardID = strings.TrimLeft(strings.Fields(input[i])[3], "#")
+	newShift.sleep = make([]bool, 60)
 	i++
 
+	time := 0
+
 	for {
-		// We always assume we start with a new Guard Line
-
-		break
+		//Check if this is last shift, aka EOF
+		if i >= len(input) {
+			break
+		}
+		// Check if we are looking at a new shift
+		if strings.Contains(input[i], "#") {
+			break
+		}
+		// Otherwise, examine the next action and act accordingly
+		if strings.Contains(input[i], "sleep") {
+			newTimeStr := strings.Fields(input[i])[1]
+			newTimeVal, _ := strconv.Atoi(strings.TrimPrefix(strings.TrimSuffix(newTimeStr, "]"), "00:"))
+			for j := time; j < newTimeVal; j++ {
+				newShift.sleep[j] = true
+			}
+			time = newTimeVal
+		}
+		if strings.Contains(input[i], "wakes") {
+			newTimeStr := strings.Fields(input[i])[1]
+			newTimeVal, _ := strconv.Atoi(strings.TrimPrefix(strings.TrimSuffix(newTimeStr, "]"), "00:"))
+			for j := time; j < newTimeVal; j++ {
+				newShift.sleep[j] = false
+			}
+			time = newTimeVal
+		}
+		i++
 	}
-
 	return newShift, i
 }
