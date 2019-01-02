632f52bfaf4a4b96efeba5697a80b04898fd43c3
hamzah-hayat <hamzahhayat@live.co.uk>
2019-01-02 17:08:48 +0000

    Insanely improve efficieny used regex
    Also finished part two

--- PolyReduce-5/main.go
+++ PolyReduce-5/main.go
@@ -4,15 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
-	"sync"
-	"unicode"
 )
 
 func main() {
-	PartOne()
-	//PartTwo()
+	//PartOne()
+	PartTwo()
 }
 
 func PartOne() {
@@ -29,28 +26,15 @@ func PartTwo() {
 	// Need to remove a letter then polyreduce the file, then store the length of the file with the letter
 	answersList := make(map[string]int)
 
-	var wg sync.WaitGroup
-	// Only run 5 threads at a time
-	goroutineChannel := make(chan struct{}, 5)
-
 	for _, letter := range allLetters {
-		wg.Add(1)
 		// First, take input and remove all letters of that type
 		newInput := strings.Replace(input, strings.ToUpper(letter), "", -1)
 		newInput = strings.Replace(newInput, strings.ToLower(letter), "", -1)
-		goroutineChannel <- struct{}{}
-		go func() {
-			defer wg.Done()
-			reduced := PolyReduction(newInput)
-			thisLetter := letter
-			answersList[thisLetter] = len(reduced)
-			fmt.Println("Finished for letter " + thisLetter + " final value was " + strconv.Itoa(len(reduced)))
-			<-goroutineChannel
-		}()
+		reduced := PolyReduction(newInput)
+		answersList[letter] = len(reduced)
+
 	}
 	fmt.Println(answersList)
-
-	wg.Wait()
 
 	lowestValue := 50000
 	lowestLetter := ""
@@ -69,46 +53,15 @@ func PartTwo() {
 // Any characters with a opposite case
 func PolyReduction(polymerString string) string {
 	newPolymer := polymerString
+	allLetters := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "x", "z"}
 
-	foundReduction := true
-	for foundReduction {
-		fmt.Println("Current string length is " + strconv.Itoa(len(newPolymer)))
-		foundReduction = false
-		for index := 0; index < len(newPolymer); index++ {
-			// First check if we are out of range
-			if index+1 >= len(newPolymer) {
-				break
-			}
-			currentChar := []rune(newPolymer)[index]
-			nextChar := []rune(newPolymer)[index+1]
-			//fmt.Println("Current Char is " + string(currentChar) + " and next char is " + string(nextChar))
-
-			if unicode.IsLower(currentChar) {
-				if unicode.IsUpper(nextChar) && unicode.ToUpper(currentChar) == unicode.ToUpper(nextChar) {
-					// Reduce these two
-					if index == 0 {
-						newPolymer = string([]rune(newPolymer)[index+2:])
-					} else {
-						newPolymer = string([]rune(newPolymer)[:index]) + string([]rune(newPolymer)[index+2:])
-					}
-					foundReduction = true
-					continue
-				}
-			}
-
-			if unicode.IsUpper(currentChar) {
-				if unicode.IsLower(nextChar) && unicode.ToUpper(currentChar) == unicode.ToUpper(nextChar) {
-					// Reduce these two
-					if index == 0 {
-						newPolymer = string([]rune(newPolymer)[index+2:])
-					} else {
-						newPolymer = string([]rune(newPolymer)[:index]) + string([]rune(newPolymer)[index+2:])
-					}
-					foundReduction = true
-					continue
-				}
-			}
-
+	newLength := len(newPolymer) + 1
+	for newLength > len(newPolymer) {
+		newLength = len(newPolymer)
+		for _, letter := range allLetters {
+			// Replace any lowercase + uppercase letters in the polymer, use length check to break out
+			newPolymer = strings.Replace(newPolymer, strings.ToUpper(letter)+strings.ToLower(letter), "", -1)
+			newPolymer = strings.Replace(newPolymer, strings.ToLower(letter)+strings.ToUpper(letter), "", -1)
 		}
 	}
 	return newPolymer
