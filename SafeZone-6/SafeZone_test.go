eebf7cb5d482968f88bb91acccea72b9e21ea004
hamzah-hayat <hamzahhayat@live.co.uk>
2019-01-04 11:10:56 +0000

    Find Largest Area Test

--- SafeZone-6/SafeZone_test.go
+++ SafeZone-6/SafeZone_test.go
@@ -5,6 +5,27 @@ import (
 	"testing"
 )
 
+func TestLargestArea(t *testing.T) {
+	inputs := [][]Point{[]Point{Point{1, 1}, Point{1, 6}, Point{8, 3}, Point{3, 4}, Point{5, 5}, Point{8, 9}}}
+	outputs := []int{17}
+
+	for i := range inputs {
+		t.Run("Test using Inputs "+strconv.Itoa(i), func(t *testing.T) {
+			testOutput := FindLargestArea(inputs[i])
+			if testOutput != outputs[i] {
+				t.Errorf("Failed on input %v", inputs[i])
+				t.Errorf("Expected result %v", outputs[i])
+				t.Errorf("Instead got %v", testOutput)
+				t.Fail()
+			} else {
+				t.Logf("Passed on input %v", inputs[i])
+				t.Logf("Expected result %v", outputs[i])
+				t.Logf("Value got was %v", testOutput)
+			}
+		})
+	}
+}
+
 func TestFindInfinitePoints(t *testing.T) {
 	inputs := [][]Point{[]Point{Point{1, 1}, Point{1, 6}, Point{8, 3}, Point{3, 4}, Point{5, 5}, Point{8, 9}}}
 	outputs := [][]Point{[]Point{Point{3, 4}, Point{5, 5}}}
@@ -27,9 +48,9 @@ func TestFindInfinitePoints(t *testing.T) {
 }
 
 func TestNearestNeighbours(t *testing.T) {
-	inputs1 := []Point{Point{18, 20}, Point{0, 0}}
-	inputs2 := [][]Point{[]Point{Point{12, 18}, Point{1, 2}}, []Point{Point{1, 1}}}
-	outputs := []Point{Point{12, 18}, Point{1, 1}}
+	inputs1 := []Point{Point{18, 20}, Point{0, 0}, Point{4, 6}}
+	inputs2 := [][]Point{[]Point{Point{12, 18}, Point{1, 2}}, []Point{Point{1, 1}}, []Point{Point{1, 1}, Point{1, 6}, Point{8, 3}, Point{3, 4}, Point{5, 5}, Point{8, 9}}}
+	outputs := []Point{Point{12, 18}, Point{1, 1}, Point{5, 5}}
 
 	for i := range inputs1 {
 		t.Run("Test using Inputs "+strconv.Itoa(i), func(t *testing.T) {
