9b6c9fe2b08ebd2a0cb8712b0eb3eccceb443618
Hamzah <hamzahhayat@live.co.uk>
2019-12-14 14:43:52 +0000

    Write test for part 1

--- 2019/14-SpaceStoichiometry/SpaceStoichiometry_test.go
+++ 2019/14-SpaceStoichiometry/SpaceStoichiometry_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestNBodyCheck_SamePositon_2(t *testing.T) {
+func TestSpaceStoichiometry_CreateReactions(t *testing.T) {
 
 	input := []string{
 		"10 ORE => 10 A",
@@ -24,6 +24,28 @@ func TestNBodyCheck_SamePositon_2(t *testing.T) {
 
 	if reactionsList[0].ingredients[0].number != reaction.ingredients[0].number || reactionsList[0].ingredients[0].name != reaction.ingredients[0].name {
 		t.Error(fmt.Sprint("Exected output ", reactionsList[0].ingredients[0], " but got ", reaction.ingredients[0]))
+	}
+
+}
+
+func TestSpaceStoichiometry_TestOreNeededSmall(t *testing.T) {
+
+	input := []string{
+		"10 ORE => 10 A",
+		"1 ORE => 1 B",
+		"7 A, 1 B => 1 C",
+		"7 A, 1 C => 1 D",
+		"7 A, 1 D => 1 E",
+		"7 A, 1 E => 1 FUEL",
+	}
+	expected := 31
+
+	reactionList := createReactions(input)
+
+	oreNeededForFuel := convertOreToFuel(reactionList)
+
+	if expected != oreNeededForFuel {
+		t.Error(fmt.Sprint("Exected output ", expected, " but got ", oreNeededForFuel))
 	}
 
 }
