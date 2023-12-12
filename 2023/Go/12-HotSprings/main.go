0a2c6c4a1e9a4e66d0c43648b3edf56a6d0f5f67
Hamzah Hayat <hamzahhayat@live.co.uk>
2023-12-12 09:01:10 +0000

    Day 12 - 2023
    Finish part 1 but not part 2

---
+++ 2023/Go/12-HotSprings/main.go
@@ -0,0 +1,248 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	methodP *string
+)
+
+func parseFlags() {
+	methodP = flag.String("method", "all", "The method/part that should be run, valid are p1,p2 and test")
+	flag.Parse()
+}
+
+func main() {
+
+	parseFlags()
+
+	switch *methodP {
+	case "all":
+		fmt.Println("Silver:" + PartOne("input"))
+		fmt.Println("Gold:" + PartTwo("input"))
+	case "p1":
+		fmt.Println("Silver:" + PartOne("input"))
+	case "p2":
+		fmt.Println("Gold:" + PartTwo("input"))
+	}
+}
+
+func PartOne(filename string) string {
+	input := readInput(filename)
+
+	possiblePatterns := 0
+	for _, v := range input {
+		split := strings.Split(v, " ")
+		gears := split[0]
+		pattern := split[1]
+
+		patterns, _ := solveGearPatterns(gears, pattern)
+
+		possiblePatterns += patterns
+	}
+
+	num := strconv.Itoa(possiblePatterns)
+
+	return num
+}
+
+func solveGearPatterns(gears, pattern string) (int, []string) {
+	var matchs []string
+	patterns := 0
+
+	possibleGearStrings := expandGears(gears)
+
+	regexString := ""
+	split := strings.Split(pattern, ",")
+	regexString += `^\.*`
+	for i, v := range split {
+		regexString += `#{` + string(v) + `}`
+
+		if i < len(split)-1 {
+			regexString += `\.+`
+		}
+	}
+	regexString += `\.*$`
+
+	r, _ := regexp.Compile(regexString)
+
+	for _, v := range possibleGearStrings {
+		if r.Match([]byte(v)) {
+			matchs = append(matchs, v+" "+pattern)
+			patterns++
+		}
+	}
+
+	return patterns, matchs
+}
+
+func expandGears(gears string) []string {
+	var possibleGearStrings []string
+
+	possibleGearStrings = expandGearsRecurse(gears)
+
+	return possibleGearStrings
+}
+
+func expandGearsRecurse(gear string) []string {
+	var possibleGearStrings []string
+	if strings.Contains(gear, "?") {
+		notGear := strings.Replace(gear, "?", ".", 1)
+		gear := strings.Replace(gear, "?", "#", 1)
+		possibleGearStrings = append(possibleGearStrings, expandGearsRecurse(notGear)...)
+		possibleGearStrings = append(possibleGearStrings, expandGearsRecurse(gear)...)
+	} else {
+		possibleGearStrings = append(possibleGearStrings, gear)
+	}
+	return possibleGearStrings
+}
+
+// To low 7405945528484
+func PartTwo(filename string) string {
+	input := readInput(filename)
+
+	possiblePatterns := 0
+	for _, v := range input {
+		split := strings.Split(v, " ")
+		gears := split[0]
+		pattern := split[1]
+
+		possiblePatterns += solveGearPatternsPart2(gears, pattern, 5)
+	}
+
+	num := strconv.Itoa(possiblePatterns)
+
+	return num
+}
+
+// Correct, but never finishes :(
+// func PartTwo(filename string) string {
+// 	input := readInput(filename)
+
+// 	possiblePatterns := 0
+// 	for _, v := range input {
+// 		split := strings.Split(v, " ")
+// 		gears := split[0]
+// 		pattern := split[1]
+
+// 		p, _ := solveGearPatterns(gears, pattern)
+// 		p2, m := solveGearPatterns(gears+"?"+gears, pattern+","+pattern)
+		
+// 		for _, v := range m {
+// 			println(v)
+// 		}
+
+// 		multiplier := p2/p
+// 		possiblePatterns += p * multiplier * multiplier * multiplier * multiplier
+// 	}
+
+// 	num := strconv.Itoa(possiblePatterns)
+
+// 	return num
+// }
+
+func unfoldGear(gear string, size int) string {
+
+	split := strings.Split(gear, " ")
+
+	gears := split[0]
+	pattern := split[1]
+
+	// Unfold by copying gears by 5 (with seperator ?)
+	// Copy pattern by 5 (with seperator ,)
+	newGears := ""
+	newPattern := ""
+
+	for i := 0; i < size; i++ {
+		newGears += gears + "?"
+		newPattern += pattern + ","
+	}
+
+	return newGears[:len(newGears)-1] + " " + newPattern[:len(newPattern)-1]
+}
+
+func unfoldGears(input []string, size int) []string {
+	var unfoldedGears []string
+
+	for _, v := range input {
+		unfoldedGears = append(unfoldedGears, unfoldGear(v, size))
+	}
+
+	return unfoldedGears
+}
+
+func solveGearPatternsPart2(gears, pattern string, unfolds int) int {
+	patterns := 0
+
+	_, firstMatchs := solveGearPatterns(gears, pattern)
+
+	for _, m := range firstMatchs {
+		secondUnfold := unfoldGearWithOrg(gears, m, 2)
+		split := strings.Split(secondUnfold, " ")
+		gears := split[0]
+		pattern := split[1]
+		_, secondMatchs := solveGearPatterns(gears, pattern)
+		patterns += len(firstMatchs) * len(secondMatchs) * len(secondMatchs) * len(secondMatchs) * len(secondMatchs)
+		break
+	}
+
+	return patterns
+}
+
+func unfoldGearWithOrg(org string, gear string, size int) string {
+
+	split := strings.Split(gear, " ")
+
+	g := split[0]
+	pattern := split[1]
+
+	// Unfold by copying gears by size (with seperator ?)
+	// Copy pattern by size (with seperator ,)
+	newGears := g + "?"
+	newPattern := pattern + ","
+
+	for i := 1; i < size; i++ {
+		newGears += org + "?"
+		newPattern += pattern + ","
+	}
+
+	return newGears[:len(newGears)-1] + " " + newPattern[:len(newPattern)-1]
+}
+
+// Read data from input.txt
+// Return the string, so that we can deal with it however
+func readInput(filename string) []string {
+
+	var input []string
+
+	f, _ := os.Open(filename + ".txt")
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+	return input
+}
+
+// Read data from input.txt
+// Return the string as int
+func readInputInt() []int {
+
+	var input []int
+
+	f, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		num, _ := strconv.Atoi(scanner.Text())
+		input = append(input, num)
+	}
+	return input
+}
